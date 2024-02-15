8541beeb61c6a3dffa14bd82bf1d4aa867c1c582
Valery Vallejo <valery.antonella19@gmail.com>
2024-02-15 00:05:06 -0500

    feat: project creation with MVC

---
+++ model/Entry.js
@@ -0,0 +1,10 @@
+class Entry {
+  constructor(order, title, comments, points) {
+    this.order = order;
+    this.title = title;
+    this.comments = comments;
+    this.points = points;
+  }
+}
+
+module.exports = Entry;
