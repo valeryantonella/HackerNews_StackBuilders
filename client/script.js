57830a4c1864d62c3783fc1430f80475d884a5de
Valery Vallejo <valery.antonella19@gmail.com>
2024-02-15 23:20:35 -0500

    feat: filters sorted by points and comments

--- client/script.js
+++ client/script.js
@@ -1,14 +1,56 @@
-async function fetchNews() {
-  const response = await fetch("/api/news");
-  const data = await response.json();
+// Función para obtener y mostrar todas las noticias
+async function fetchAndDisplayAllNews() {
+  try {
+    const response = await fetch("/api/news");
+    const data = await response.json();
 
-  const newsList = document.getElementById("newsList");
-  data.forEach((newsItem) => {
-    const li = document.createElement("li");
-    li.textContent = ` ${newsItem.order} ${newsItem.title} - ${newsItem.points}, ${newsItem.comments}`;
-    newsList.appendChild(li);
-  });
+    const newsList = document.getElementById("newsList");
+    // Limpiar la lista antes de agregar nuevas noticias
+    newsList.innerHTML = "";
+
+    data.forEach((newsItem) => {
+      const li = document.createElement("li");
+      li.textContent = ` ${newsItem.order} ${newsItem.title} - Points: ${newsItem.points}, Comments: ${newsItem.comments}`;
+      newsList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
 }
 
-// Fetch news data when the page loads
-window.onload = fetchNews;
+// Función para realizar la solicitud y mostrar las noticias filtradas
+async function fetchAndDisplayFilteredNews(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const newsList = document.getElementById("newsList");
+    // Limpiar la lista antes de agregar nuevas noticias filtradas
+    newsList.innerHTML = "";
+
+    data.forEach((newsItem) => {
+      const li = document.createElement("li");
+      li.textContent = ` ${newsItem.order} ${newsItem.title} - Points: ${newsItem.points}, Comments: ${newsItem.comments}`;
+      newsList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error fetching filtered news:", error);
+  }
+}
+
+// Mostrar todas las noticias al cargar la página
+window.onload = fetchAndDisplayAllNews;
+
+// Función para filtrar por más de 5 palabras en el título
+document
+  .getElementById("filterByMoreThan5WordsBtn")
+  .addEventListener("click", () => {
+    fetchAndDisplayFilteredNews("/api/news/filterByComments");
+  });
+
+// Función para filtrar por menos o igual a 5 palabras en el título
+document
+  .getElementById("filterByLessThanOrEqual5WordsBtn")
+  .addEventListener("click", () => {
+    fetchAndDisplayFilteredNews("/api/news/filterByPoints");
+  });
